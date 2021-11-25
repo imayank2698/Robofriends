02d32e4654e2b048ae670d69a59100975bc4689f
imayank2698 <mcmayank8@gmail.com>
2021-11-25 12:57:32 +0530

    Updated to hooks code

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,57 +1,46 @@
-import React,{Component} from 'react';
+import React,{useState,useEffect} from 'react';
 import CardArray from '../components/CardArray';
 import SearchBox from '../components/SearchField';
-//import {robots} from '../robots.js';
 import Scroll from '../components/Scroll';
 import './App.css';
 import ErrorBoundary from '../components/ErrorBoundary';
 
+function App(){
 
-class App extends Component{
-    constructor(){
-        super();
-        this.state = {
-            searchField : "",
-            robots : []
-        }
+    const [robots,setRobots] = useState([]);
+    const [searchField,setSearchField] = useState('');
+
+    const SearchEvent = (event)=>{
+        setSearchField(event.target.value);
     }
 
-    componentDidMount(){
+    useEffect(()=>{
         fetch("https://jsonplaceholder.typicode.com/users")
         .then(response=>{return response.json()})
-        .then(users=>this.setState({robots:users}));
+        .then(users=>setRobots(users));
+    },[]);
+
+    const filtered_robots = robots.filter(robot=>{
+        return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    });
+
+    if(!this.state.robots){
+        return <h1>Loading</h1>
+    }else{
+        return(
+            <div className="tc pa2 ma2">
+                <h1 className="f1">Robofriends</h1>
+                <SearchBox className="tc pa4 ma2" SearchEvent = {SearchEvent}/>
+                <Scroll>
+                    <ErrorBoundary>
+                        <CardArray robots = {filtered_robots}/>
+                    </ErrorBoundary>
+                </Scroll>    
+            </div>
+         );
+
     }
 
-    SearchEvent = (event)=>{
-        //console.log(event.target.value);
-        this.setState({searchField:event.target.value});
-
-    }
-
-    render(){
-        const filtered_robots = this.state.robots.filter(robot=>{
-            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
-        });
-        //console.log(filtered_robots);
-
-        if(!this.state.robots){
-            return <h1>Loading</h1>
-        }else{
-            return(
-                <div className="tc pa2 ma2">
-                    <h1 className="f1">Robofriends</h1>
-                    <SearchBox className="tc pa4 ma2" SearchEvent = {this.SearchEvent}/>
-                    <Scroll>
-                        <ErrorBoundary>
-                            <CardArray robots = {filtered_robots}/>
-                        </ErrorBoundary>
-                    </Scroll>    
-                </div>
-             );
-
-        }
-    }
-    
 }
 
 export default App;
